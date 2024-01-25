5a941db11548c6c775412faf797a57ed3b589175
MagicalJellyfish <lupe030803@gmail.com>
2024-01-25 17:12:00 +0100

    minor cleanup, added actions/roll dice roll interpreter

---
+++ brokenHeart/Controllers/ActionsController.cs
@@ -0,0 +1,20 @@
+﻿using Dice;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace brokenHeart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActionsController : ControllerBase
+    {
+        public ActionsController() { }
+
+        [HttpGet]
+        [Route("roll")]
+        public async Task<List<string>> Roll(string rollString)
+        {
+            return Roller.Roll(rollString).ToString().Split("=>").Select(x => x.Trim()).ToList();
+        }
+    }
+}
