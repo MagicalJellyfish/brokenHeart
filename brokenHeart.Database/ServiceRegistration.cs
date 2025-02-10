8f226c9f0db737fcf7c6c1c92954b7c29566f846
MagicalJellyfish <lupe030803@gmail.com>
2025-02-10 21:28:50 +0100

    fix: SignalR character change notification

---
+++ brokenHeart.Database/ServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using brokenHeart.Database.Utility;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace brokenHeart.Database
+{
+    public static class ServiceRegistration
+    {
+        public static void RegisterServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<DatabaseEventEmitter>();
+        }
+    }
+}
