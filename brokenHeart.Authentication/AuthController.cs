35b82c0f53dc7ab5e119d49f25b597b371228cf7
MagicalJellyfish <lupe030803@gmail.com>
2025-02-23 13:33:07 +0100

    refactor: remove "api" from auth endpoints

--- brokenHeart.Authentication/AuthController.cs
+++ brokenHeart.Authentication/AuthController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace brokenHeart.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
