b737343f2635dd3e91fa5a4e43abbabc9df76157
MagicalJellyfish <lupe030803@gmail.com>
2026-04-16 17:50:39 +0200

    refactor: use defined class for add participant endpoint response

---
+++ brokenHeart.Models/brokenHand/AddParticipantMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace brokenHeart.Models.brokenHand
+{
+    public class AddParticipantMessage
+    {
+        public string Name { get; set; }
+        public string Shortcut { get; set; }
+        public int InitRoll { get; set; }
+    }
+}
